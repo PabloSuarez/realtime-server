a62d1ac13a96178472bb1d2a2c1a7c314f60ac13
Pablo Suarez <pablo.suarez@influenster.com>
2019-08-21 01:33:26 -0300

    base 2

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
+const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const axios = require('axios');
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 80;
 const routes = require('./routes/');
 const app = express();
 
 app.use(routes);
+app.use(express.static(path.join(__dirname, '../')));
 
 const server = http.createServer(app);
 const io = socketIo(server);
-const getApiAndEmit = async socket => {
-  try {
-    // Getting the data
-    const res = await axios.get('https://api.github.com/users/PabloSuarez/repos');
-    // Emitting a new message. It will be consumed by the client
-    socket.emit('FromAPI', res.data);
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
-
-let interval;
-io.on('connection', socket => {
-  console.log('New client connected');
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 10000);
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
-
+// const getApiAndEmit = async socket => {
+//   try {
+//     // Getting the data
+//     const res = await axios.get('https://api.github.com/users/PabloSuarez/repos');
+//     // Emitting a new message. It will be consumed by the client
+//     socket.emit('FromAPI', res.data);
+//   } catch (error) {
+//     console.error(`Error: ${error.code}`);
+//   }
+// };
+//
+// let interval;
+// io.on('connection', socket => {
+//   console.log('New client connected');
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 10000);
+//   socket.on('disconnect', () => {
+//     console.log('Client disconnected');
+//   });
+// });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
